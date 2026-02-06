e2bd1ad11b093d17a662b1793105bd8fde5902de
Cătălin Frâncu <cata@francu.com>
2026-02-06 22:30:55 +0200

    Implement a geometry problem.

--- src/geometry/arpa-and-an-exam.cpp
+++ src/geometry/arpa-and-an-exam.cpp
@@ -1,1 +1,17 @@
-TODO
+#include <iostream>
+
+long long dist2(long long ax, long long ay, long long bx, long long by) {
+  return ((ax - bx) * (ax - bx) + (ay - by) * (ay - by));
+}
+
+int main() {
+  long long ax, ay, bx, by, cx, cy;
+  std::cin >> ax >> ay >> bx >> by >> cx >> cy;
+
+  bool noncollinear = ((cx - ax) * (by - ay) - (cy - ay) * (bx - ax) != 0);
+  bool equidistant = dist2(ax, ay, bx, by) == dist2(bx, by, cx, cy);
+
+  std::cout << ((noncollinear && equidistant) ? "Yes\n" : "No\n");
+
+  return 0;
+}
